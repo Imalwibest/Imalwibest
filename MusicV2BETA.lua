43857e318b89ad3ffd3cb871cc392cfe34e65ba6
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-04-08 23:17:46 +0700

    Update MusicV2BETA.lua

--- MusicV2BETA.lua
+++ MusicV2BETA.lua
@@ -1,6 +1,8 @@
+-- ui libary 
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/UI-th%20Library%20v0.1.2"))()
 local window = Lib:CreateWindow("Alwi Hub | MusicTestV2")
 local tab = window:CreateTab("Main")
+local tabs = window:CreateTab("Button Play") 
 tab:Search()
 
 -- music id you can add id from here
@@ -78,6 +80,260 @@ local volumeSlider = tab:CreateSlider("Volume", 0, 100, volume, function(value)
     volume = value
     if currentMusic then
         currentMusic.Volume = volume / 100 -- Set volume based on 100 scale
+    end
+end)
+
+-- toggle main play and stop 
+tabs:Search()
+local musicToggle = tabs:CreateToggle("HR - EEYUH", false, function(state)
+    local musicId = "rbxassetid://16190782181" 
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("kirkiimad - i love (tiktok version)", false, function(state)
+    local musicId = "rbxassetid://15689439571"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("FNAF 2 Song", false, function(state)
+    local musicId = "rbxassetid://6913550990"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("A Heart Worth Saving", false, function(state)
+    local musicId = "rbxassetid://1836807218"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("We Could Be Kings (a)", false, function(state)
+    local musicId = "rbxassetid://1840030788"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("Parry Gripp - Taco Bot 3000", false, function(state)
+    local musicId = "rbxassetid://9245552700"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("WRLD - Hang Up (feat. Savoi)", false, function(state)
+    local musicId = "WRLD - Hang Up (feat. Savoi)"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("Night Out", false, function(state)
+    local musicId = "rbxassetid://6703926669"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("VIP Me (a)", false, function(state)
+    local musicId = "rbxassetid://1839920066"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("Tranical Endl", false, function(state)
+    local musicId = "rbxassetid://1837984979"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("Wherever You Are (a)", false, function(state)
+    local musicId = "rbxassetid://6872126938"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
+    end
+end)
+
+local musicToggle = tabs:CreateToggle("LaserGun_10", false, function(state)
+    local musicId = "rbxassetid://1843324953"
+    local volume = 100
+
+    if state then
+        if not currentMusic then
+            currentMusic = Instance.new("Sound", game)
+            currentMusic.SoundId = musicId
+            currentMusic.Volume = volume
+            currentMusic:Play()
+        end
+        musicPlaying = true
+    else
+        if currentMusic then
+            currentMusic:Stop()
+            currentMusic = nil
+        end
+        musicPlaying = false
     end
 end)
 
