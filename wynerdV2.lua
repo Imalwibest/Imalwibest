67557d4344be40fc13adc375731e5681aaa7ee53
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-04-25 21:26:17 +0700

    Update wynerdV2.lua

--- wynerdV2.lua
+++ wynerdV2.lua
@@ -1,6 +1,12 @@
 
 game.StarterGui:SetCore("SendNotification", {
-    Title = "whike be fixing issue";
+    Title = "while be fixing issue";
     Text = "Maintaine please wait Alwi hub acc github got hacked"; 
+    Duration = 15;
+})
+wait() 
+game.StarterGui:SetCore("SendNotification", {
+    Title = "im sorry";
+    Text = "i have nice day i took me fixed it 10minz just wait thank you"; 
     Duration = 15;
 })
