a5eff1114c6b214f05cfb608c256a5e328a1d6a3
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-05-06 17:38:03 +0700

    Update S.F.S.T.lua

--- S.F.S.T.lua
+++ S.F.S.T.lua
@@ -1,48 +1,56 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/UI-th%20Library%20v0.1.2"))()
-local window = Lib:CreateWindow("                                                                   ALWI HUB | S.F.S.T)
+local window = Lib:CreateWindow("ALWI HUB | S.F.S.T") 
 local tab = window:CreateTab("Main")
 local tabs = window:CreateTab("Misc")
 local w = window:CreateTab("info/credit")
+
 tab:CreateToggle("Auto Spin",false,function(claim) 
-_G.Boykisser = claim
-while _G.Boykisser == true do
-    wait() 
-game:GetService("ReplicatedStorage"):WaitForChild("RequestRoll"):InvokeServer()
-end
+    _G.Boykisser = claim
+    while _G.Boykisser == true do
+        wait() 
+        game:GetService("ReplicatedStorage"):WaitForChild("RequestRoll"):InvokeServer()
+    end
+end)
+
+tab:CreateToggle("Auto FF",false,function(sex) 
+    _G.rylumysex = sex
+    while _G.rylumysex == true do
+        wait() 
+        game:GetService("ReplicatedStorage"):WaitForChild("ActivateForcefield"):FireServer()
+    end
 end) 
 
-tab:CreateToggle("Auto Safe Zone!",false,function(claim
-
-end
+tab:CreateToggle("Freeze",false,function(LMAO) 
+    for _, player in pairs(game.Players:GetPlayers()) do
+        local character = player.Character
+        if character then
+            for _, part in ipairs(character:GetDescendants()) do
+                if part:IsA("BasePart") then
+                    part.Anchored = LMAO
+                end
+            end
+        end
+    end
 end) 
 
 tab:CreateButton("Buy product Gamepass [Free]", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Imalwibest/main/wynerdV2.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Imalwibest/main/wynerdV2.lua",true))()
 end) 
-  
+
 tab:CreateButton("AntiAFK", function()
-game.StarterGui:SetCore("SendNotification", {
-    Title = "AntiAfk V1";
-    Text = "Turn On"; 
-    Duration = 30;
-})
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "AntiAfk V1";
+        Text = "Turn On"; 
+        Duration = 30;
+    })
 
-
-local VirtualUser = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:Connect(function()
-    VirtualUser:CaptureController()VirtualUser:ClickButton2(Vector2.new())
+    local VirtualUser = game:GetService("VirtualUser")
+    game:GetService("Players").LocalPlayer.Idled:Connect(function()
+        VirtualUser:CaptureController()
+        VirtualUser:ClickButton2(Vector2.new())
+    end)
 end)
-end)
 
-    
-
--- credit by ikura thanks you arigato
--- Define variables with default values
-local ClickingSpeed = 0
-local x, y, m = 55, 65.5, 1
-local loopActive = false
-
--- Auto-close error function
 tabs:CreateToggle("Auto Close Error",false,function(value)
     loopActive = value
     spawn(function()
@@ -61,7 +69,6 @@ tabs:CreateToggle("Auto Close Error",false,function(value)
     end)
 end)
 
--- Auto-buy function
 tabs:CreateToggle("Auto Buy",false,function(value)
     loopActive = value
     spawn(function()
@@ -85,23 +92,23 @@ tabs:CreateToggle("Auto Buy",false,function(value)
             end
         end
     end)
+end)
 
 w:CreateToggle("Hide Player",false,function(Fu) 
-        _G.Fu = Fu
-while _G.Fu and  task.wait() do
-
-for i,v in pairs(game.Players:GetPlayers()) do
-if v.Name ~= game.Players.LocalPlayer.Name and v.Character then
-v.Character:Destroy()
-end
-end
-end
+    _G.Fu = Fu
+    while _G.Fu and  task.wait() do
+        for i,v in pairs(game.Players:GetPlayers()) do
+            if v.Name ~= game.Players.LocalPlayer.Name and v.Character then
+                v.Character:Destroy()
+            end
+        end
+    end
 end) 
 
 w:CreateButton("Console", function() 
-game:GetService("StarterGui"):SetCore("DevConsoleVisible",true)
+    game:GetService("StarterGui"):SetCore("DevConsoleVisible",true)
 end)
 
 w:CreateButton("Join My discord", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Imalwibest/main/Discord%20sex%20mom%20cum"))()
-end) 
+    loadstring(game:HttpGet(""))() 
+end)
