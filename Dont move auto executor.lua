e1dabfd4c173c79bf7c0a09418eee535e4ae07c6
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-05-09 08:25:50 +0700

    Update Dont move auto executor.lua

--- Dont move auto executor.lua
+++ Dont move auto executor.lua
@@ -5,19 +5,20 @@ game:GetService("Players").LocalPlayer.Idled:Connect(function()
 end)
 
 game.StarterGui:SetCore("SendNotification", {
-    Title = "Auto executor hass been bypass";
-    Text = "Auto click bubble and more antiafk"; 
+    Title = "Auto executor completely success";
+    Text = "You know Alwi is love furry :3"; 
     Duration = 30;
 })
 
 _G.AutoExecutorPublicSources = true
 while _G.AutoExecutorPublicSources == true do
     wait() 
-game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.SpinService.RF.Spin:InvokeServer() 
-wait() 
 game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("QuestService"):WaitForChild("RE"):WaitForChild("FriendInvite"):FireServer()
 wait() 
 game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.5.1").knit.Services.BubbleService.RE.Clicked:FireServer()
+wait() 
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("QuestService"):WaitForChild("RE"):WaitForChild("ClaimQuest"):FireServer()
 end
+
 
 
