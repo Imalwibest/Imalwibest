f1fda49af012f351e5e8cfa2fde154b09e008d43
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-05-24 19:56:46 +0700

    Create Classic Event Roblox.lua

---
+++ Classic Event Roblox.lua
@@ -0,0 +1,176 @@
+local Library =loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+local Window = Library.CreateLib("Roblox The Classic Script", "Sentinel")
+local Tab = Window:NewTab("Universal Script")
+local Section = Tab:NewSection("Game Name")
+-- 1
+Section:NewButton("Dress To Impress", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicDressToImpress", true))()
+end) 
+Section:NewButton("Teleport Dress To Impress ", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(14056754882, player)
+end
+end) 
+-- 2
+Section:NewButton("Bedwars", "?", function()
+local Section = Tab:NewSection("Tix Esp")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicBedWars", true))()
+end)
+local Section = Tab:NewSection("Peguin Event For token")
+local InfiniteJumpEnabled = true
+game:GetService("UserInputService").JumpRequest:connect(function()
+    if InfiniteJumpEnabled then
+        game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+    end
+end)
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+end) 
+Section:NewButton("Teleport To Bedwars", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(6872265039, player)
+end
+end) 
+-- 3
+Section:NewButton("Arsenal", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicArsenal", true))()
+end) 
+
+Section:NewButton("Teleport To Arsenal", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(286090429, player)
+end
+end) 
+
+-- 4
+Section:NewButton("Bee Srawn Sim", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicBeeSwarm", true))()
+end) 
+Section:NewButton("Teleport Bee Srawn Sim", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(1537690962, player)
+end
+end) 
+
+-- 5
+Section:NewButton("The Classic", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/TheClassic", true))()
+end) 
+Section:NewButton("Teleport The classic", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(17427651911, player)
+end
+end) 
+
+-- 6
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+                                  
+end)
+-- 7
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+                                  
+end)
+
+
+-- 7
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+
+end)
+
+-- 8
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+
+end) 
+
+-- 9
+
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+
+end) 
+
+-- 10
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+
+end) 
+
+-- 11
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+
+end) 
+
+-- 12
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+
+end) 
+
+-- 13
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+
+end) 
+
+
+-- 14
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+
+end) 
+
+-- 15
+Section:NewButton(" ", "?", function()
+
+end) 
+Section:NewButton(" ", "?", function()
+
+end) 
+
+                              
