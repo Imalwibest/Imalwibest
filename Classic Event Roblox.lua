beb46833fd981856e0936bcb80b4afbc6fbd1ec3
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-05-24 20:52:31 +0700

    Update Classic Event Roblox.lua

--- Classic Event Roblox.lua
+++ Classic Event Roblox.lua
@@ -3,16 +3,16 @@ local Window = Library.CreateLib("Roblox The Classic Script", "Sentinel")
 local Tab = Window:NewTab("Universal Script")
 local Section = Tab:NewSection("Game Name")
 -- 1
-Section:NewButton("Dress To Impress", "?", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicDressToImpress", true))()
+Section:NewButton("The Classic", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/TheClassic", true))()
 end) 
-Section:NewButton("Teleport Dress To Impress ", "?", function()
+Section:NewButton("Teleport The classic", "?", function()
 local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
 
 local player = Players.LocalPlayer
 if player then
-    TeleportService:Teleport(14056754882, player)
+    TeleportService:Teleport(17427651911, player)
 end
 end) 
 -- 2
@@ -21,6 +21,7 @@ local Section = Tab:NewSection("Tix Esp")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicBedWars", true))()
 end)
 local Section = Tab:NewSection("Peguin Event For token")
+Section:NewButton("VapeV4 And flyjump", "?", function()
 local InfiniteJumpEnabled = true
 game:GetService("UserInputService").JumpRequest:connect(function()
     if InfiniteJumpEnabled then
@@ -70,107 +71,174 @@ end
 end) 
 
 -- 5
-Section:NewButton("The Classic", "?", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/TheClassic", true))()
+Section:NewButton("Dress To Impress", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicDressToImpress", true))()
 end) 
-Section:NewButton("Teleport The classic", "?", function()
+Section:NewButton("Teleport Dress To Impress ", "?", function()
 local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
 
 local player = Players.LocalPlayer
 if player then
-    TeleportService:Teleport(17427651911, player)
+    TeleportService:Teleport(14056754882, player)
 end
 end) 
 
 -- 6
-Section:NewButton(" ", "?", function()
-
+Section:NewButton("A Dusty Trip Classic", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicAdustyTrip", true))()
 end) 
-Section:NewButton(" ", "?", function()
-                                  
+Section:NewButton("Teleport A Dusty Trio classic", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(16389395869, player)
+end
 end)
 -- 7
-Section:NewButton(" ", "?", function()
-
+Section:NewButton("LiveTopia Classic", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicLivetopia", true))()
 end) 
-Section:NewButton(" ", "?", function()
-                                  
+Section:NewButton("Teleport LiveTopia Classic", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(6737970321, player)
+end                                  
 end)
 
-
--- 7
-Section:NewButton(" ", "?", function()
-
-end) 
-Section:NewButton(" ", "?", function()
-
-end)
 
 -- 8
-Section:NewButton(" ", "?", function()
-
+Section:NewButton("GunFight script 1", "?", function()
+loadstring(game:HttpGet('https://github.com/ToraScript/Script/blob/main/ClassicGunfight'))()
+end)
+Section:NewButton("GunFight script 2", "?", function()
+loadstring(game:HttpGet(('https://pastefy.app/FL5mxhtj/raw'),true))()
 end) 
-Section:NewButton(" ", "?", function()
+Section:NewButton("Teleport GunFight", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
 
-end) 
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(14518422161, player)
+end
+end)
 
 -- 9
-
-Section:NewButton(" ", "?", function()
-
+Section:NewButton("Classic ClipIt", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicClipIt", true))()
 end) 
-Section:NewButton(" ", "?", function()
+Section:NewButton("Teleport Classic ClipIt", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
 
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(11765402359, player)
+end
 end) 
 
 -- 10
-Section:NewButton(" ", "?", function()
 
+Section:NewButton("Classic Toilet Tower", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicToiletTower", true))()
 end) 
-Section:NewButton(" ", "?", function()
+Section:NewButton("Teleport Classic Toilet Tower", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
 
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Telepor(13775256536, player)
+end
 end) 
 
 -- 11
-Section:NewButton(" ", "?", function()
-
+Section:NewButton("Restaurant Tycoon", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicRestaurant", true))()
 end) 
-Section:NewButton(" ", "?", function()
+Section:NewButton("Teleport Restaurant Tycoon", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
 
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(3398014311, player)
+end
 end) 
 
 -- 12
-Section:NewButton(" ", "?", function()
-
+Section:NewButton("Classic Driving Empire", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicDrivingEmpire", true))()
 end) 
-Section:NewButton(" ", "?", function()
+Section:NewButton("Teleport Classic Driving Empire", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
 
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(3351674303, player)
+end
 end) 
 
 -- 13
-Section:NewButton(" ", "?", function()
-
+Section:NewButton("Dragon Adventure!", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicDragon", true))()
 end) 
 Section:NewButton(" ", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
 
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(3475397644, player)
+end
 end) 
-
 
 -- 14
-Section:NewButton(" ", "?", function()
-
+Section:NewButton("Adopt me", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicAdoptMe", true))()
 end) 
-Section:NewButton(" ", "?", function()
+Section:NewButton("Teleport Adopt Me", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
 
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(920587237, player)
+end
 end) 
+
 
 -- 15
-Section:NewButton(" ", "?", function()
-
-end) 
-Section:NewButton(" ", "?", function()
-
+Section:NewButton("Blade Ball", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraScript/Script/main/ClassicBladeBall", true))()
 end) 
 
-                              
+Section:NewButton("Teleport Blade ball", "?", function()
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer
+if player then
+    TeleportService:Teleport(13772394625, player)
+end
+end) 
+
+local Tab = Window:NewTab("Credit")
+local Section = Tab:NewSection("Alwi/byeveryone")
+local Section = Tab:NewSection("Update Add all game! and more!")
+Section:NewButton("Console", "?", function()
+game:GetService("StarterGui"):SetCore("DevConsoleVisible",true)
+end)
+
+Section:NewButton("My discord", "?", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Imalwibest/main/Discord%20sex%20mom%20cum"))()
+end) 
+
+
