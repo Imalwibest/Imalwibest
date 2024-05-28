f5f2e2a2520b030cfafd59abc397954291ae781a
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-05-28 22:04:09 +0700

    Update Auto Re enter roblox config.lua

--- Auto Re enter roblox config.lua
+++ Auto Re enter roblox config.lua
@@ -1,36 +1,36 @@
 -- Made By Byeveryone/alwi#3737 This source script For rejoin after kicked or lost connection join my discord https://discord.com/invite/RmKR5STDnG
 
-local function FallbackTeleport()
+local function ikura()
     local g = game
     pcall(function()
         g:GetService("TeleportService"):TeleportToPlaceInstance(g.PlaceId, g.JobId, g.Players.LocalPlayer)
     end)
 end
 
-local function TryRejoinIfPrompt()
+local function rylu()
     local s, d = pcall(function()
         return game:GetService("CoreGui"):WaitForChild("RobloxPromptGui"):WaitForChild("promptOverlay")
     end)
 
     if s then
         d.DescendantAdded:Connect(function()
-            spawn(FallbackTeleport)
+            spawn(ikura)
         end)
 
         if #d:GetChildren() > 0 then
-            spawn(FallbackTeleport)
+            spawn(ikura)
         end
     else
         warn("RobloxPromptGui or promptOverlay not found.")
     end
 end
 
-local function OnPlayerExit(plr)
+local function alwi(plr)
     if plr == game.Players.LocalPlayer then
         game:GetService('TeleportService'):Teleport(game.PlaceId)
     end
 end
 
-game.Players.LocalPlayer.CharacterRemoving:Connect(FallbackTeleport)
-game.Players.PlayerRemoving:Connect(OnPlayerExit)
-spawn(TryRejoinIfPrompt)
+game.Players.LocalPlayer.CharacterRemoving:Connect(ikura)
+game.Players.PlayerRemoving:Connect(alwi)
+spawn(rylu)
