c654c7ece24fbfe6583dedccdf3f6d8d0c4db26e
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-10-15 03:15:31 +0700

    Create Baby da skid nigga.lua

---
+++ Baby da skid nigga.lua
@@ -0,0 +1,70 @@
+if game.PlaceId == 14567590300 then
+
+    local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Imalwibest/refs/heads/main/Fluxus%20Ui.lib"))();
+    local Window = lib:CreateWindow("Bake Da Hub V1 | Made by grilyt");
+
+    local MainTab = Window:NewTab("Main");
+    local MainSection = MainTab:AddSection("Main Section");
+    local InfoSection = MainTab:AddSection("Information/Support");
+
+    MainSection:AddButton("[OP] Simple", "This is OP VERY MUCHHHH", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/CHdtQbEX"))()
+    end)
+
+    MainSection:AddButton("[OP] Invisible GUI", "This is OP VERY MUCHHHH", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/0Ben1/fe./dbb6ce6f6cee4f7a5c9e20d7b88e83db2a93bf25/Invisible%20GUI'))()
+    end)
+
+    MainSection:AddButton("Noclip GUI", "Noclip Toggle UI", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/6T4skKuj"))()
+    end)
+
+    MainSection:AddButton("Fly GUI V3", "Fly Toggle UI, V3", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/bHa6HDFc"))()
+    end)
+
+    MainSection:AddButton("Infinite Yield (ESP)", "Read the name", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+    end)
+
+    MainSection:AddButton("Use Infinite Yield Delta Mobile", "For Delta users", function()
+    end)
+
+    local ChefsTab = Window:NewTab("Chefs")
+    local ChefsSection = ChefsTab:AddSection("This is for the Chefs.")
+
+    ChefsSection:AddButton("Speed 32", "Makes your speed to 32", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/Fgcns0St"))()
+    end)
+
+    ChefsSection:AddButton("[OP] Invisible GUI (Teleport To)", "This is OP VERY MUCHHHH", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/0Ben1/fe./dbb6ce6f6cee4f7a5c9e20d7b88e83db2a93bf25/Invisible%20GUI'))()
+    end)
+
+    local BabiesTab = Window:NewTab("Babies")
+    local BabiesSection = BabiesTab:AddSection("This is for the Babies.")
+
+    BabiesSection:AddButton("[OP] Hipheight 20", "This is OP VERY MUCHHHH", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/s7Cx3aCU"))()
+    end)
+
+    BabiesSection:AddButton("[Useful] Get back on ground", "Maybe OP.. ¯\\_(ツ)_/¯", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/Q5tr0AuT"))()
+    end)
+
+    BabiesSection:AddButton("Float GUI", "Wow Arceuz XE!!!", function()
+        loadstring(game:HttpGet('https://pastebin.com/raw/K9aw4neF'))()
+    end)
+
+    BabiesSection:AddButton("Infinite Yield (tpwalk 1-5)", "Read the name", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+    end)
+
+    BabiesSection:AddButton("Use Infinite Yield Delta Mobile", "For Delta users", function()
+    end)
+
+    InfoSection:AddButton("Discord Server", "Copy Discord Link", function()
+        setclipboard("https://discord.gg/cpXUTmMXXd")
+    end)
+
+end
